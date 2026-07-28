f41ede9c56c84e2c1d1c866ae87b9d34a63a43ec
makc_0 <makcim565@ya.ru>
2026-07-28 06:15:15 +0500

    feature/input
    Логика для генератора рандомного списка студентов

--- src/com/team/studentsorter/input/RandomDataFiller.java
+++ src/com/team/studentsorter/input/RandomDataFiller.java
@@ -1,6 +1,8 @@
 package com.team.studentsorter.input;
 
 import com.team.studentsorter.model.Student;
+import com.team.studentsorter.validation.StudentValidator;
+
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -9,18 +11,23 @@ import java.util.stream.Stream;
 public class RandomDataFiller implements DataFiller {
     private final Random random = new Random();
 
+    // TODO: проверить
     @Override
     public List<Student> fill(int size) {
-        // TODO (Максим): заполнение ОБЯЗАТЕЛЬНО через стрим:
-        // return Stream.generate(this::randomStudent)
-        //              .limit(size)
-        //              .collect(Collectors.toList());
-        return null;
+        return Stream.generate(this::randomStudent)
+                     .limit(size)
+                     .collect(Collectors.toList());
     }
 
+    // TODO: проверить
     private Student randomStudent() {
-        // генерация в ДОПУСТИМЫХ диапазонах из StudentValidator,
-        // балл округлить до 1 знака: Math.round(x * 10) / 10.0
-        return null;
+        return new Student.Builder()
+            .groupNumber(
+                random.nextInt(StudentValidator.MIN_GROUP, StudentValidator.MAX_GROUP + 1)
+            ).averageGrade(
+                random.nextInt((int)(StudentValidator.MIN_GRADE*10), (int)(StudentValidator.MIN_GRADE*10) + 1) * 0.1d
+            ).recordBookNumber(
+                random.nextInt(StudentValidator.MIN_RECORD_BOOK, StudentValidator.MAX_RECORD_BOOK + 1)
+            ).build();
     }
 }
