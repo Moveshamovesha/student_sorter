d138d06dcccf91e2c2c6059c5c6c95165d1666f2
Sh@mil89 <shaidullin.sha@gmail.com>
2026-08-02 03:10:08 +0500

    fix SortTest

--- test/com/team/studentsorter/BinarySearchTest.java
+++ test/com/team/studentsorter/BinarySearchTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class BinarySearchTest {
 
-    public static void main(String[] args) {
+    public static void run() {
         testSearchExistingElement();
         testSearchFirstElement();
         testSearchLastElement();
